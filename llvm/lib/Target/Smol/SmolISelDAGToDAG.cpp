aac4e04084451a9cafd5ff9ce02cd6f7c2d14d0c
asu <sdelang@sdelang.fr>
2023-08-27 00:50:14 +0200

    Added offset loads

--- llvm/lib/Target/Smol/SmolISelDAGToDAG.cpp
+++ llvm/lib/Target/Smol/SmolISelDAGToDAG.cpp
@@ -15,7 +15,9 @@
 #include "SmolRegisterInfo.h"
 #include "llvm/CodeGen/MachineFunction.h"
 #include "llvm/CodeGen/SelectionDAGISel.h"
+#include "llvm/CodeGen/SelectionDAGNodes.h"
 #include "llvm/Support/KnownBits.h"
+#include <cstdint>
 
 using namespace llvm;
 
@@ -25,6 +27,36 @@ char SmolDAGToDAGISel::ID = 0;
 
 bool SmolDAGToDAGISel::runOnMachineFunction(MachineFunction &MF) {
   return SelectionDAGISel::runOnMachineFunction(MF);
+}
+
+bool SmolDAGToDAGISel::SelectAddrRegImm(SDValue Addr, SDValue &Base,
+                                        SDValue &Offset, bool SExt,
+                                        int64_t TargetSize) {
+  SDLoc DL(Addr);
+  MVT VT = Addr.getSimpleValueType();
+
+  if (CurDAG->isBaseWithConstantOffset(Addr)) {
+    auto *const CNode = cast<ConstantSDNode>(Addr.getOperand(1));
+
+    uint64_t CVal;
+    bool Fits;
+
+    if (SExt) {
+      CVal = CNode->getSExtValue();
+      Fits = (isIntN(TargetSize, CVal));
+    } else {
+      CVal = CNode->getZExtValue();
+      Fits = (isUIntN(TargetSize, CVal));
+    }
+    
+    if (Fits) {
+      Base = Addr.getOperand(0);
+      Offset = CurDAG->getTargetConstant(CVal, DL, VT);
+      return true;
+    }
+  }
+
+  return false;
 }
 
 void SmolDAGToDAGISel::Select(SDNode *Node) {
